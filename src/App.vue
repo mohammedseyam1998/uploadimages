645731f39285ceea81c1e4ffd5fcffb805e94edb
mohammedseyam1998 <m05921033@gmail.com>
2020-01-25 16:35:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="card card-default">
+                    <div class="card-header">Upload Your Images</div>
+
+                    <div class="card-body">
+                        <image-uploader></image-uploader>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ImageUploader from './components/ImageUploader.vue'
+
+export default {
+  name: 'app',
+ 
+        components: {
+            ImageUploader
+        }
+}
+</script>
+
+<style>
+
+</style>
